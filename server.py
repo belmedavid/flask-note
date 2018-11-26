486eefbe6df790e749121a147d6009987966b899
David Belme <belmedavid@gmail.com>
2018-11-26 19:02:22 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key = "somesecretkey"
+
+
+@app.route("/")
+def route_index():
+    note_text = None
+    if "note" in session:
+        note_text = session['note']
+    return render_template('index.html', note=note_text)
+
+
+@app.route("/edit_note")
+def route_edit():
+    note_text = None
+    if "note" in session:
+        note_text = session["note"]
+    return render_template("edit.html", note=note_text)
+
+
+@app.route("/edit_note", methods=['POST'])
+def route_save():
+    session["note"] = request.form["note"]
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=8000
+    )
